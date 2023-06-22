3da3220916fea7e61c0a286f0531dc69205f4853
Conrad Fitzgerald <cfitz012@uottawa.ca>
2023-06-22 15:38:07 -0400

    Write scaffolding for min/max heapq Array implementation

---
+++ src/heapq_array.rb
@@ -0,0 +1,59 @@
+class Array
+  def heapq_min_heapify()
+
+  end
+
+  def heapq_min_enqueue(n)
+
+  end
+
+  def heapq_min_deque()
+
+  end
+
+  def heapq_max_heapify()
+
+  end
+
+  def heapq_max_enqueue(n)
+
+  end
+
+  def heapq_max_deque()
+
+  end
+
+  def heapq_min_update(old_val, new_val)
+
+  end
+
+  def heapq_max_update(old_val, new_val)
+
+  end
+
+  private:
+
+  def heapq_get_parent(n)
+
+  end
+
+  def heapq_get_children(n)
+
+  end
+
+  def bubble_min(idx)
+
+  end
+
+  def sink_min(idx)
+
+  end
+
+  def bubble_max(idx)
+
+  end
+
+  def sink_max(idx)
+
+  end
+end
