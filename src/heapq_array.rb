9188ed440ee24e36c6ae2be9dc0dcb34c583c21d
Conrad Fitzgerald <cfitz012@uottawa.ca>
2023-06-22 16:20:13 -0400

    Implement heapq helper methods

--- src/heapq_array.rb
+++ src/heapq_array.rb
@@ -34,11 +34,14 @@ class Array
   # private #comment out while running tests
 
   def heapq_get_parent(n)
-
+    return nil if n <= 0
+    (n - 1) / 2
   end
 
   def heapq_get_children(n)
-
+    return nil if n < 0
+    children = [n * 2 + 1, n * 2 + 2]
+    children.select { |child| child < self.length }
   end
 
   def heapq_bubble_min(idx)
@@ -58,9 +61,16 @@ class Array
   end
 
   def heapq_smaller_child(arr)
-
+    arr.each { |idx| return nil if idx >= self.length }
+    return nil if arr.length == 0
+    return arr[0] if arr.length == 1
+    (self[arr[0]] < self[arr[1]]) ? arr[0] : arr[1]
   end
-  def heapq_larger_child(arr)
 
+  def heapq_larger_child(arr)
+    arr.each { |idx| return nil if idx >= self.length }
+    return nil if arr.length == 0
+    return arr[0] if arr.length == 1
+    (self[arr[0]] > self[arr[1]]) ? arr[0] : arr[1]
   end
 end
